e220e48d3e4c4bb1da8b6d7c0a5c744add9bd08f
Venu Gopal <nkvenugopal@gmail.com>
2023-03-02 05:20:03 +0530

    Updated

--- cartrack/application-javascript/cartrack.js
+++ cartrack/application-javascript/cartrack.js
@@ -13,7 +13,7 @@ const { buildCCPOrg1, buildCCPOrg2, buildWallet, prettyJSONString} = require('..
 const myChannel = 'mychannel';
 const myChaincodeName = 'cartrack';
 
-async function createCar(ccp,wallet,user,carId, name, model) {
+async function trackCar(ccp,wallet,user,carId) {
 	try {
 
 		const gateway = new Gateway();
@@ -25,16 +25,13 @@ async function createCar(ccp,wallet,user,carId, name, model) {
 		const network = await gateway.getNetwork(myChannel);
 		const contract = network.getContract(myChaincodeName);
 
-		let statefulTxn = contract.createTransaction('ManufactorCar');
-
-		console.log('\n--> Submit Transaction: Propose Manufactor Car');
-		await statefulTxn.submit(carId,carDetails);
-		console.log('*** Result: committed');
+		let result = await contract.evaluateTransaction('trackCarById',carId);
+		console.log('*** Result: Auction: ' + prettyJSONString(result.toString()));
 
 		
 		gateway.disconnect();
 	} catch (error) {
-		console.error(`******** FAILED to Manufactor Car: ${error}`);
+		console.error(`******** FAILED to get history of CarId: ${error}`);
 	}
 }
 
@@ -42,29 +39,29 @@ async function main() {
 	try {
 
 		if (process.argv[2] === undefined || process.argv[3] === undefined ||
-            process.argv[4] === undefined || process.argv[5] === undefined) {
-			console.log('Usage: node cartrack.js org userID carId name model');
+            process.argv[4] === undefined ) {
+			console.log('Usage: node cartrack.js org userID carId');
 			process.exit(1);
 		}
 
 		const org = process.argv[2];
 		const user = process.argv[3];
-		const auctionID = process.argv[4];
-		const item = process.argv[5];
+		const carId = process.argv[4];
+		
 
 		if (org === 'Org1' || org === 'org1') {
 			const ccp = buildCCPOrg1();
 			const walletPath = path.join(__dirname, 'wallet/org1');
 			const wallet = await buildWallet(Wallets, walletPath);
-			await createCar(ccp,wallet,user,carId,name,model);
+			await trackCar(ccp,wallet,user,carId);
 		}
 		else if (org === 'Org2' || org === 'org2') {
 			const ccp = buildCCPOrg2();
 			const walletPath = path.join(__dirname, 'wallet/org2');
 			const wallet = await buildWallet(Wallets, walletPath);
-			await createCar(ccp,wallet,user,carId,name,model);
+			await trackCar(ccp,wallet,user,carId);
 		}  else {
-			console.log('Usage: node cartrack.js org userID carId name model');
+			console.log('Usage: node cartrack.js org userID carId');
 			console.log('Org must be Org1 or Org2');
 		}
 	} catch (error) {
